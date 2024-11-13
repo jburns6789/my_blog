8c53445450d558952115c707dda1f75bea61166e
[joel] <[jbur645@wgu.edu]>
2024-11-12 23:09:08 -0500

    added forms.py and finishing up views.py

--- my_blog/views.py
+++ my_blog/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import CommentForm
 
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status='published')
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'my_blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
+    return render(request,'my_blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comments': new_comment,
+                   'comment_form': comment_form})
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect(post.get_absolute_url())
+    else:
+        form = CommentForm
+    return render(request, 'my_blog/post/add_comment.html', {'form': form, 'post': post})
 
