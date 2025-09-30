37d42803397678e059a71fb807d4368a1070dbb8
[joel] <[jbur645@wgu.edu]>
2025-09-29 23:01:50 -0400

    updating templates

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -9,7 +9,7 @@ app_name = 'my_blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('<int:post_id>/comment/', views.add_comment, name='add_comment'),
+    #path('<int:post_id>/comment/', views.add_comment, name='add_comment'),
 
     path('api/posts/', PostListAPIView.as_view(), name='api_post_list'),
     path('api/posts/<slug:slug>', PostDetailAPIView.as_view(), name='api_post_detail'),
