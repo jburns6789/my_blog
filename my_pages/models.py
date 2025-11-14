6810fa9b521c14a1f2c14927a9cd68e01b0ac7c4
[joel] <[jbur645@wgu.edu]>
2025-11-14 06:58:51 -0500

    form debugging

--- my_pages/models.py
+++ my_pages/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
-    message = models.DateTimeField(default=timezone.now)
+    message = models.TextField(max_length=200)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
